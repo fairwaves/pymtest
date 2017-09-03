883d5659a76fcac7000fc764f810487c6b04288b
Alexander Chemeris <Alexander.Chemeris@gmail.com>
2017-09-03 10:52:16 +0300

    cmd57-scripts: Uss sshpass for umsite-check-uptime.sh

--- umsite-check-uptime.sh
+++ umsite-check-uptime.sh
@@ -2,12 +2,16 @@
 
 IPS=$*
 
+export SSHPASS=fairwaves
+
 if test -z "$IPS" ; then
   echo "usage: $0 ip_address [ip_address] [ip_address] ..."
   exit 1
 fi
 
+ssh-keygen -R ${IP} >/dev/null 2>&1
+
 for IP in $IPS ; do
-  cat helper/umsite-check-uptime.py | ssh fairwaves@$IP "sudo python -"
+  cat helper/umsite-check-uptime.py | sshpass -e ssh -o "StrictHostKeyChecking no" fairwaves@$IP "sudo python -"
   echo
 done
